a782a14200eb2fbc6257c11ff51df0bacca86dc6
Vicente Gayoso <106035065+nyzerx@users.noreply.github.com>
2023-05-11 22:10:47 -0400

    Update BackSpringApplication.java

--- src/main/java/com/ObjetosUbb/BackSpringApplication.java
+++ src/main/java/com/ObjetosUbb/BackSpringApplication.java
@@ -23,7 +23,7 @@ public class BackSpringApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://frontangular-production.up.railway.app/").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://objetosubb.up.railway.app/").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
