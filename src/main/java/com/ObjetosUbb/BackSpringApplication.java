ce8ec6575c3a34f532a197939fca191d62e9a796
econtreras <econtreras@anticipa.cl>
2023-06-06 00:54:15 -0400

    updatev5

--- src/main/java/com/ObjetosUbb/BackSpringApplication.java
+++ src/main/java/com/ObjetosUbb/BackSpringApplication.java
@@ -19,7 +19,7 @@ public class BackSpringApplication {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200/").allowedMethods("*").allowedHeaders("*");
+				registry.addMapping("/**").allowedOrigins("https://frontangular-production.up.railway.app").allowedMethods("*").allowedHeaders("*");
 			}
 		};
 	}
